698f1bdb7c747fcb7a053701833088e34fb57b4b
manyuanbin <yuanbin.man@gmail.com>
2024-10-13 13:20:21 -0400

    update OrderMapper

---
+++ code/backend/src/main/java/edu/bu/cs673/secondhand/controller/OrderController.java
@@ -0,0 +1,7 @@
+package edu.bu.cs673.secondhand.controller;/***
+Name:  Yuanbin(Stan) Man, ex-IBM, ex-Alibaba, AI Engineer in CV/DL/MLSys
+Email: ybinman@bu.edu         
+DateTime: 10/13/24-13:12           
+Publications:  https://arxiv.org/abs/2312.16385; https://ieeexplore.ieee.org/document/10379821;                 
+*****/public class OrderController {
+}
---
+++ code/backend/src/main/java/edu/bu/cs673/secondhand/utils/IdFactoryUtil.java
@@ -0,0 +1,7 @@
+package edu.bu.cs673.secondhand.utils;/***
+Name:  Yuanbin(Stan) Man, ex-IBM, ex-Alibaba, AI Engineer in CV/DL/MLSys
+Email: ybinman@bu.edu         
+DateTime: 10/13/24-13:16           
+Publications:  https://arxiv.org/abs/2312.16385; https://ieeexplore.ieee.org/document/10379821;                 
+*****/public class IdFactoryUtil {
+}
