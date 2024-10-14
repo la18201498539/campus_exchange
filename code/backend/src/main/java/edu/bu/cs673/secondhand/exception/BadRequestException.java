973148d6b60ebab41dd3c948fcbbdcf9d7c6120e
Srujana N <srujana@Srujanas-MacBook-Pro.local>
2024-10-13 21:07:13 -0400

    Github Bugfix #7 along with BadRequestException and Message Service Unit test cases

---
+++ code/backend/src/main/java/edu/bu/cs673/secondhand/exception/BadRequestException.java
@@ -0,0 +1,12 @@
+package edu.bu.cs673.secondhand.exception;
+
+public class BadRequestException extends RuntimeException {
+
+  public BadRequestException() {
+    super();
+  }
+
+  public BadRequestException(String message) {
+    super(message);
+  }
+}
