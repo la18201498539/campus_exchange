0e47a4d6397b0492cb674420e8bdea1f59a97638
la18201498539 <57754170+la18201498539@users.noreply.github.com>
2024-11-03 12:23:40 -0500

    Update request.js

--- code/frontend/src/utils/request.js
+++ code/frontend/src/utils/request.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 
 const service = axios.create({
     timeout: 5000,
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://47.252.50.70:8080',
     withCredentials: true
 });
 
