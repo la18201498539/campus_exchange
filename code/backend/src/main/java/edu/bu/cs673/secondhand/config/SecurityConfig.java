e2940d651e57205e1a124d9d23cbf4b2dbe44522
la18201498539 <57754170+la18201498539@users.noreply.github.com>
2024-11-05 22:05:25 -0500

    Update SecurityConfig.java

--- code/backend/src/main/java/edu/bu/cs673/secondhand/config/SecurityConfig.java
+++ code/backend/src/main/java/edu/bu/cs673/secondhand/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8081"));
+        // config.setAllowedOrigins(List.of("http://localhost:8081"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
@@ -77,7 +77,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:8081"));
+        // config.setAllowedOrigins(List.of("http://localhost:8081"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
